e0fa27e1fcfe06d86a20993c0c6766e4c2f647a1
Gustavo Cruz <gc7340048@gmail.com>
2023-08-29 23:04:54 -0300

    Login Creado para PP en react

--- Login/src/App.jsx
+++ Login/src/App.jsx
@@ -7,39 +7,15 @@ import Login2 from './component/Login2'
 function App() {
 //Estado:
 
-const [valorInput, setValorInput] = useState('hola');
-
-const onChange = (evento) => {
-  const valor = evento.target.value;
-  setValorInput(valor);
-} 
-
-const [gifs, setGifs] = useState();
-
-const getGifs = async () => {
-  const url = 'https://api.giphy.com/v1/gifs/search?api_key=n17gD7jKMSMqNzV5C7inG97OvPHlC49N&q=initial d'
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data)
-}
-
-const onSubmit = (evento) => {
-  evento.prevetDefault()
-  console.log(evento)
-}
-
-
-
-
 //JSX:
   return (
     <>
     <div>
-
-      <form onSubmit={onSubmit}>
-        <input value={valorInput} onChange={onChange}/>
-      </form>
-
+      <h1>Login</h1>
+      <p>Nombre</p>
+      <Login></Login>
+      <p>Contraseña</p>
+      <Login2></Login2>
     </div>
     </>
   )
--- Login/src/component/Login.jsx
+++ Login/src/component/Login.jsx
@@ -5,7 +5,6 @@ import styles from './Login.module.css'
 function Login(){
     return(
         <div className={styles.formulario}>
-                <h1>Login</h1>
                 <input type="text" placeholder="Nombre de usuario"/>
         </div>
 
